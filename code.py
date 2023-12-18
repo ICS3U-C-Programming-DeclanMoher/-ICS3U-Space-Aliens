56a9f5a1589db19e6af17b61b8d613a1da53a386
Declan Moher <declan.moher@stu.ocsb.ca>
2023-12-18 14:01:35 -0500

    Added more to code.py

--- code.py
+++ code.py
@@ -1,20 +1,52 @@
 import ugame
 import stage 
 
+import constants
+
 def game_scene():
-    image_bank_backround =stage.Bank.from_bmp16("space_aliens_background.bmp")
-     
+    #importing background from files into 
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+#setting size for ship and background
     background = stage.Grid(image_bank_background, 10, 8)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-
+#setting layers and size of the game 
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
 
+    #calling to render the game 
     game.render_block()
 
     while True:
+    #updating the every second
+        game.render_sprites ([ship])
+        game.tick()
+
+    #user inputs and setting buttons to actions
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+
         pass
 
 
-    if __name__ == "__main__":
-        game_scene()
+if __name__ == "__main__":
+    game_scene()
