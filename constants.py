c96a01d82f55983543f8dadae846d69fb58c0ed9
Declan Moher <declan.moher@stu.ocsb.ca>
2023-12-20 12:31:53 -0500

    updates to constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+#pybagde is set to the size of 160x128 aswell with sprites being 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -6,3 +7,11 @@ SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+#button state for maintianing information
+button_state ={
+    "button_up" : "up",
+"button_just_pressed": "just pressed",
+"button_still_pressed" : "still pressed",
+"button_released" : "released"
+}
