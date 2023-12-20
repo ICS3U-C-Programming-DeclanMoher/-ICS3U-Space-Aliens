c0a637666954e409f5bccae008acb8a77de081d2
Declan Moher <declan.moher@stu.ocsb.ca>
2023-12-20 13:13:22 -0500

    Added code.py updates

--- code.py
+++ code.py
@@ -7,29 +7,44 @@ def game_scene():
     #importing background from files into 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    
+    #adds text objects
+    text = []
+    text1 = stage.text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
 
+    text2 = stage.text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
 
-#buttons that keep state information
+    #sets the background to image 0 in image bank 
+    background = stage.Grid(image_bank_background, constants.SCREEN_X,constants.SCREEN_Y)
+    
+    #buttons that keep state information
     a_button = constants.button_state["button_up"]
     b_button = constants.button_state["button_up"]
     start_button = constants.button_state["button_up"]
     select_button = constants.button_state["button_up"]
 
-#get sound ready 
+    #get sound ready 
     pew_sound = open("pew.wav", 'rb')
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
 
-#setting size for ship and background
+    #setting size for ship and background
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
     ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y -(2 * constants.SPRITE_SIZE))
     alien = stage.Sprite(image_bank_sprites, 9,
     int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
     16)
-#setting layers and size of the game 
+
+    #setting layers and size of the game 
     game = stage.Stage(ugame.display, constants.FPS)
-    game.layers = [ship] + [alien] + [background]
+    game.layers = text + [background]
 
     #calling to render the game 
     game.render_block()
@@ -59,7 +74,7 @@ def game_scene():
             print('Select')
         if keys & ugame.K_RIGHT != 0:
             if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
-                ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
+                ship.move(ship.x + 1, ship.y)
             else:
                 ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
 
@@ -73,9 +88,12 @@ def game_scene():
                 pass
             if keys & ugame.K_DOWN != 0:
                 pass
-#playing pew sound when a button pressed 
-            if a_button == constants.button_state["button_just_pressed"]:
-                sound.play(pew_sound)
+    #playing pew sound when a button pressed 
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
+
+            game.render_sprites([ship] + [alien])
+            game.tick()
 #rerender sprites 
             game.render_sprites([ship] + [alien])
         if keys & ugame.K_X:
@@ -86,27 +104,21 @@ def game_scene():
             pass
         if keys & ugame.K_SELECT:
             pass
-
 #creating borders
         if keys & ugame.K_RIGHT:
             if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
                 ship.move(ship.x + 1, ship.y)
         else:
             pass
-
 #creating borders
         if keys & ugame.K_LEFT:
             if ship.x >= 0:
                 ship.move(ship.x - 1, ship.y)
         else:
             pass
-
-
         if keys & ugame.K_UP:
             pass
         if keys & ugame.K_DOWN:
-            pass
-
             pass
 if __name__ == "__main__":
     game_scene()
